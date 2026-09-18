f1f98983885c365f7ed3dc0bbd8b18ded5665868
MHijazi <mohamadhijazi91@gmail.com>
2026-09-18 11:42:39 +0300

    sample json

--- newapp/shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
+++ newapp/shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6137fac5c2a5d80b4b162fe18d070e92f110118b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a0ff2953de88b66eab4dfee3fd3c0fbd949c3d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
